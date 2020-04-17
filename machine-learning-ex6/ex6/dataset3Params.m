c6f213085bad549e58eec893d2d19170f293f3e0
zxdclyz <1515685918@qq.com>
2020-04-17 19:25:16 +0800

    add ans of ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_list = [0.01,0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_list = [0.01,0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+n1 = 8;
+n2 = 8;
+error = zeros(n1, n2);
 
+for i = 1 : n1
+    for j = 1 : n2
+        model= svmTrain(X, y, C_list(i),...
+            @(x1, x2) gaussianKernel(x1, x2, sigma_list(j)));
+        predictions = svmPredict(model, Xval);
+        error(i, j) = mean(double(predictions ~= yval));
+    end
+end
 
+[x, y] = find(error == min(error,[],'all'));
 
-
-
+C = C_list(x);
+sigma = sigma_list(y);
 % =========================================================================
 
 end
